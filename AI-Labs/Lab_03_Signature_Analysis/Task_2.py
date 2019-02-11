c50fd5478a657290c32fc12c630ed022e7440145
Kamran Janjua <kamranejaz98@gmail.com>
2019-02-11 15:37:20 +0500

    Update Task_2.py

--- AI-Labs/Lab_03_Signature_Analysis/Task_2.py
+++ AI-Labs/Lab_03_Signature_Analysis/Task_2.py
@@ -1,31 +1,21 @@
-from PIL import Image, ImageDraw
 import numpy as np 
+import cv2 
 """
-We implement bounding box algorithm for the OTSU binarized image.
+We implement centroid computing algorithm for the OTSU binarized image.
 """
-img_file = "otsu_bin_sig.png"
-img = Image.open(img_file)
-img = img.convert("RGBA")
-width, height = img.size
-image_data = img.load()
-pixel = [width, height]
-end = [0,0]
-
-for y in xrange(height):
-	for x in xrange(width):
-	  if image_data[x, y][0] < 200 and image_data[x, y][1] < 200 and image_data[x, y][2] < 200:
-		if x < pixel[0]:
-		  pixel[0] = x
-		if y < pixel[1]:
-		  pixel[1] = y
-		if x > end[0]:
-		  end[0] = x
-		if y > end[1]:
-		  end[1] = y
-
-bounding_box = (pixel[0], pixel[1], end[0], end[1])
-bounded_box_img = img.crop(bounding_box)
-bounded_box_img.save("cropped_image.png")
-draw_rect = ImageDraw.Draw(img)
-draw_rect.rectangle(((pixel[0], pixel[1]),(end[0], end[1])), outline="black")
-img.save("detected_box.png")
+def centroid_computation(img_file):
+	img = cv2.imread(img_file, 0)
+	height, width = img.shape
+	cx, cy, n = 0,0, 0
+	for x in range(height):
+		for y in range(width):
+			if img[x,y] == 0:
+				cx = cx + x 
+				cy = cy + y
+				n += 1
+	cx = cx // n
+	cy = cy // n 
+	print("Centeroid: ", " cX: ", cx, " cY: ", cy)
+	centeroid_image = cv2.circle(img, (cy, cx), 10, 200, -1)
+	cv2.imwrite('centeroid_image.png', centeroid_image)
+	return cx, cy
