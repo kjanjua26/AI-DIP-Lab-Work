ddd349ff46d24cf01634719fbf2884e54a6d631e
Kamran Janjua <kamranejaz98@gmail.com>
2019-02-11 15:38:41 +0500

    Update Task_1.py

--- AI-Labs/Lab_03_Signature_Analysis/Task_1.py
+++ AI-Labs/Lab_03_Signature_Analysis/Task_1.py
@@ -1,13 +1,33 @@
-import cv2 
 import numpy as np 
+import cv2 
+"""
+We implement bounding box algorithm for the OTSU binarized image.
+"""
 
-"""
-  Reading image, and converting to black and white. 
-  We use OTSU binarization for the task.
-"""
-img_file = "test.jpg"
-img = cv2.imread(img_file)
-img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-cv2.imwrite("grey_sig.png", img_gray)
-retval2,threshold = cv2.threshold(img_gray,125,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
-cv2.imwrite("otsu_bin_sig.png", threshold)
+def bounding_box(img_file):
+	img = cv2.imread(img_file, 0)
+	height, width = img.shape
+	print(height, width)
+	left, right = width, 0
+	top, bottom = height, 0
+
+	for x in range(height-1):
+		for y in range(width-1):
+			color = img[x,y]
+			if color == 0:
+				if x > right:
+					right = x
+				if x < left:
+					left = x 
+				if y > bottom:
+					bottom = y
+				if y < top:
+					top = y
+
+
+	print("The bounding box is: ")
+	print("Top: ", top, " Bottom: ", bottom, " Left: ", left, " Right: ", right)
+	bounding_box = cv2.rectangle(img, (top, left), (bottom, right), (0,0,0), 3)
+	bounding_box_image = img[left:right, top:bottom]
+	cv2.imwrite('detected_box.png', bounding_box)
+	return top, bottom, right, left
